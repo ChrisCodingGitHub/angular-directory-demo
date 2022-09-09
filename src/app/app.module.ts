b3a77c067a955727412a9a02ce852e4d93638069
Chris Wright <wright.christopher.gary@gmail.com>
2022-09-08 23:55:34 -0600

    TASK-9 Moving modules folder into app folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { DirectoryConfigModule } from '../modules/directory-config/directory-config.module';
-import { DirectorySharedModule } from '../modules/directory-shared/directory-shared.module';
-import { DirectoryModule } from '../modules/directory/directory.module';
+import { DirectoryConfigModule } from './modules/directory-config/directory-config.module';
+import { DirectorySharedModule } from './modules/directory-shared/directory-shared.module';
+import { DirectoryModule } from './modules/directory/directory.module';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
