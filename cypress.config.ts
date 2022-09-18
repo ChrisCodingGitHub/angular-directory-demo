e49abf004695d7cc0c9a89f29e9c1806f0af032c
Chris Wright <wright.christopher.gary@gmail.com>
2022-09-17 19:56:53 -0600

    TASK-6 Attempting pipeline fix

--- cypress.config.ts
+++ cypress.config.ts
@@ -4,7 +4,10 @@ export default defineConfig({
   
   e2e: {
     'baseUrl': 'http://localhost:4200',
-    supportFile: false
+    supportFile: false,
+    pageLoadTimeout: 60000,
+    defaultCommandTimeout: 60000,
+    chromeWebSecurity: false
   },
   
   
